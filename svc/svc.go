bfd3210befcb8aca22c53e3eada9e5b19f4ca290
Augustinas Bacvinka <augustinas.bacvinka@gmail.com>
2022-09-19 22:04:20 +0300

    Implementation of subscribing and publishing messages to topics

--- svc/svc.go
+++ svc/svc.go
@@ -1,56 +1,135 @@
 package svc
 
-type Subscription struct {
+import (
+	"errors"
+	"sync"
+	"sync/atomic"
+)
+
+const (
+	EventChanBound = 10
+	SubscriptionTimeoutSecs = 30
+)
+
+type Event struct {
 	id uint64
-	eventsC chan string
+	msg string
 }
 
-func (s *Subscription) Listen() <-chan string {
+type Subscription struct {
+	id uint64
+	eventsC chan Event 
+	onClose func()
+}
+
+func (s *Subscription) Listen() <-chan Event{
 	return s.eventsC
 }
 
+func (s *Subscription) Publish(id uint64, msg string) {
+	s.eventsC <- Event{id:id, msg:msg}
+}
+
 func (s *Subscription) Close() {
-	// TODO
+	s.onClose()
 	close(s.eventsC)
+}
+
+type Topic struct {
+	subscriptions map[uint64]*Subscription
+	mu sync.RWMutex
+	sub_idx uint64
+}
+
+func (t *Topic) Subscribe() *Subscription {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	atomic.AddUint64(&t.sub_idx, 1)
+	id := atomic.LoadUint64(&t.sub_idx) 
+
+	sub := &Subscription{
+		id: id,
+		eventsC: make(chan Event, EventChanBound),
+		onClose: t.onClose(id),
+		
+	}
+
+	t.subscriptions[id] = sub
+	return sub
+}
+
+func (t *Topic) Publish(id uint64, msg string) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	for _, sub := range t.subscriptions {
+		sub.Publish(id, msg)
+	}
+}
+
+func (t *Topic) onClose(id uint64) func() {
+	return func() {
+		t.mu.Lock()
+		defer t.mu.Unlock()
+
+		delete(t.subscriptions, id)
+	}
 }
 
 type EventService interface {
 	Subscribe(topic string) (*Subscription, error)
-	Publish(topic, message string) uint64
-	Spawn()
+	Publish(topic, message string) (uint64, error)
 }
 
 type service struct {
-	subscriptions map[string]map[uint64]*Subscription
+	subscriptions map[string]*Topic
+	event_idx uint64
+	mu sync.RWMutex
 }
 
 func NewService() EventService {
+	return newService()
+}
+
+func newService() *service {
 	return &service{
-		subscriptions: make(map[string]map[uint64]*Subscription),
+		subscriptions: make(map[string]*Topic),
 	}
 }
 
 func (s *service) Subscribe(topic string) (*Subscription, error) {
 	s.provision(topic)
 
-	id := uint64(0)
-	sub := &Subscription{
-		id: id,
-		eventsC: make(chan string),
-	}
-
-	s.subscriptions[topic][id] = sub
+	sub := s.subscriptions[topic].Subscribe()
 	return sub, nil
 }
 
 func (s *service) provision(topic string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	if _, ok := s.subscriptions[topic]; !ok {
-		s.subscriptions[topic] = make(map[uint64]*Subscription)
+		s.subscriptions[topic] = &Topic{
+			subscriptions: make(map[uint64]*Subscription),
+		}
 	}
 }
 
-func (s *service) Publish(topic, message string) uint64 {
-	return 0
+func (s *service) Publish(topic, message string) (uint64, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	// If nobody's subscribed to the channel, ignore the message.
+	if _, ok := s.subscriptions[topic]; !ok {
+		return 0, errors.New("non existent topic")
+	}
+
+	atomic.AddUint64(&s.event_idx, 1)
+	id := atomic.LoadUint64(&s.event_idx) 
+
+	s.subscriptions[topic].Publish(id, message)
+
+	return id, nil 
 }
 
-func (s *service) Spawn() {}
--- svc/svc_test.go
+++ svc/svc_test.go
@@ -9,27 +9,34 @@ import (
 )
 
 func TestSvcSubscription(t *testing.T) {
-	service := NewService()
-	go service.Spawn()
+	service := newService()
 
 	sub, err := service.Subscribe("test")
 	assert.NoError(t, err)
 
 	select {
-	case msg := <-sub.Listen():
-		fmt.Printf("msg %s\n", msg)
+	case event := <-sub.Listen():
+		fmt.Printf("msg %s", event.msg)
 	case <-time.After(1 * time.Second):
 		sub.Close()
 	}
 
-	//assert.Equal(t, len(service.topics), 1)
+	assert.Equal(t, len(service.subscriptions), 1)
 }
 
 func TestSvcPublishEvent(t *testing.T) {
-	service := NewService()
-	go service.Spawn()
+	service := newService()
 
-	//id := service.Publish("topic", "data")
-	//assert.Equal(t, len(service.topics), 1)
+	_, err := service.Publish("topic", "data")
+	assert.Error(t, err) // non existent topic
+	assert.Equal(t, len(service.subscriptions), 0)
+
+	_, err = service.Subscribe("topic")
+	assert.NoError(t, err)
+
+	_, err = service.Publish("topic", "data")
+	assert.NoError(t, err)
+
+	assert.Equal(t, len(service.subscriptions), 1)
 }
 
